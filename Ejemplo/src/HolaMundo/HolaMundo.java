56fb64916a0ae9489f0a3d74acf672ced2a28320
Holver <holverp@gmail.com>
2018-04-22 13:47:18 -0500

    commit 22/04/2018 1:47 pm

--- Ejemplo/src/HolaMundo/HolaMundo.java
+++ Ejemplo/src/HolaMundo/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Holver ...");
+		System.out.println("Hola Holver Patiño ...");
 	}
 
 }
